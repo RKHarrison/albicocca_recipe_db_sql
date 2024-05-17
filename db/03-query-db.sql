fdb4123685143f40505ac0377aa3184e600c2031
Richard Harrison <r.harr.k@gmail.com>
2024-05-17 16:33:13 +0100

    queries our recipes for each course

--- db/03-query-db.sql
+++ db/03-query-db.sql
@@ -8,6 +8,10 @@ JOIN ingredients i ON i.ingredient_id = ri.ingredient_id
 GROUP BY recipe_name;
 
 
--- \echo '\nHere are our available recipes for each course:\n'
--- SELECT course_name, ARRAY_AGG(recipe_name)
--- FROM course
+\echo '\nHere are our available recipes for each course:\n'
+SELECT course_name, array_to_string(ARRAY_AGG(recipe_name), ', ')
+FROM courses c
+JOIN recipes r ON r.course_id = c.course_id
+GROUP BY course_name, c.course_id
+ORDER BY c.course_id; 
+
